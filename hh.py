ab4bc41a56144ed951525857002cbcd0abba4cdf
Puneeth456 <shivanpuneeth50@gmail.com>
2020-04-28 11:46:02 +0530

    first commit

---
+++ hh.py
@@ -0,0 +1,46 @@
+
+a=100
+
+def f1():
+    print("inside f1")
+
+
+import re
+
+s=input("Enter email Id")
+m=re.fullmatch("[a-zA-Z0-9_.]*@gmail.com",s)
+if m!=None:
+    print("valid email Id")
+else:
+    print("Invalid email Id")
+
+
+
+import re
+s=input("Enter the email Id")
+m=re.fullmatch("[a-zA-Z0-9_.]{5-25}@gmail.com",s)
+if m!=None:
+    print("valid email Id")
+else:
+    print("Invalid email Id")
+
+
+Regular expression is for client side or server side validation
+
+
+Client side validation should be handled using html or java script
+
+
+client side validation avoids  more request to the server by making  initial check
+
+
+import re
+
+s=input("enter the mobile number")
+m=re.fullmatch("91-([0-9]){10}",s)
+if m!=None:
+    print("Valid email Id")
+else:
+    print("Invalid email Id")
+
+
