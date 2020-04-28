a5bb08bf251defbd1c7193e989a4c3477919843f
Puneeth456 <shivanpuneeth50@gmail.com>
2020-04-28 13:47:59 +0530

    third commit

--- hh.py
+++ hh.py
@@ -1,57 +1,153 @@
+#
+# a=100
+#
+# def f1():
+#     print("inside f1")
+#
+#
+# import re
+#
+# s=input("Enter email Id")
+# m=re.fullmatch("[a-zA-Z0-9_.]*@gmail.com",s)
+# if m!=None:
+#     print("valid email Id")
+# else:
+#     print("Invalid email Id")
+#
+#
+#
+# import re
+# s=input("Enter the email Id")
+# m=re.fullmatch("[a-zA-Z0-9_.]{5-25}@gmail.com",s)
+# if m!=None:
+#     print("valid email Id")
+# else:
+#     print("Invalid email Id")
+#
+#
+# Regular expression is for client side or server side validation
+#
+#
+# Client side validation should be handled using html or java script
+#
+#
+# client side validation avoids  more request to the server by making  initial check
+#
+#
+# import re
+#
+# s=input("enter the mobile number")
+# m=re.fullmatch("91-([0-9]){10}",s)
+# if m!=None:
+#     print("Valid email Id")
+# else:
+#     print("Invalid email Id")
+#
+#
+# import re
+#
+# s=input("enter the mobile number")
+# m=re.fullmatch("91-([0-9]){10}",s)
+# if m!=None:
+#     print("Valid email Id")
+# else:
+#     print("Invalid email Id")
+#
+#
+# print("Program ended")
+#
 
-a=100
+#
+# Whenever developer make some changes to the repository(push the code)
+#
+# 1) Deployment should be happening automatically
+# 2) make sure deployment is successfull
+# 3) once deployment is done then run the automation script
 
-def f1():
-    print("inside f1")
-
-
-import re
-
-s=input("Enter email Id")
-m=re.fullmatch("[a-zA-Z0-9_.]*@gmail.com",s)
-if m!=None:
-    print("valid email Id")
-else:
-    print("Invalid email Id")
+#
+# Jenkins.war file should be run for each and every  not instance
+#
+# Jenkins.war file should be run for each and every not instance
 
 
 
-import re
-s=input("Enter the email Id")
-m=re.fullmatch("[a-zA-Z0-9_.]{5-25}@gmail.com",s)
-if m!=None:
-    print("valid email Id")
-else:
-    print("Invalid email Id")
+#
+# from abc import *
+#
+#
+# class flight(ABC):
+#     @abstractmethod
+#     def run(self):
+#         pass
+#
+#
+#
+#
+# @abstractmethod
+# def fly(self):
+#     pass
+#
+#
+# f1=flight()
+
+#
+# In python there is no concept of interface in python.
+#
+# but we can simulate interface concept by making all method abstract.
+#
+#
+# Locators:
+# web page
+# web element:
+#
+# web page can have a user name field and password field(text box) this kind of fileds will be called as web element.
+# each and every web element will have attribute name and attribute
+# not value
+# there is no limit for attribute name and attribute value it purely depends on the ui developer
+#
+#
+# webelement              tag name                        mandatory an and av
+#
+# text field          input                        type=text, type=email, type ="password"
+#
+# button                 button /input(            type=submit
+#
+#
+# dropdown                select
+#
+# Radio                  input             type =radio
+#
+#
+# images                      img
+#
+#
+# tables                      table
+#
+# link                a                       href
+#
 
 
-Regular expression is for client side or server side validation
+#
+#
+# text filed              input   type="email" type="password" type="text"
+#
+# button                 input/button  type=submit
+#
+# dropdown                select
+#
+# Radio                     input    type=raido
+#
+#
+# images      imng
+#
+# table      tab;exit(
+#
+#
+#     link            a   href
+# )
 
 
-Client side validation should be handled using html or java script
 
 
-client side validation avoids  more request to the server by making  initial check
 
 
-import re
-
-s=input("enter the mobile number")
-m=re.fullmatch("91-([0-9]){10}",s)
-if m!=None:
-    print("Valid email Id")
-else:
-    print("Invalid email Id")
-
-
-import re
-
-s=input("enter the mobile number")
-m=re.fullmatch("91-([0-9]){10}",s)
-if m!=None:
-    print("Valid email Id")
-else:
-    print("Invalid email Id")
-
-
-print("Program ended")
